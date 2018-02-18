875773c6178eafb5dea6f328147c56df8ca5c63b
Marco Rossi <developer@marco-rossi.com>
2018-02-18 12:25:05 +0100

    Add editing the resulting *.exe with LARGEADDRESSAWARE

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,10 @@ msiexec /a pandoc-%PKG_VERSION%-windows.msi /qb TARGETDIR=%TEMP% || exit 1
 
 if not exist %SCRIPTS% mkdir %SCRIPTS% || exit 1
 
+IF "%EDITBINPATH%" == "" (
+    echo "Path for editbin not defined, skip applying /LARGEADDRESSAWARE"
+) else (
+    "%EDITBINPATH%\editbin.exe" /LARGEADDRESSAWARE %TEMP%\Pandoc\*.exe || exit 1
+)
+
 copy %TEMP%\Pandoc\*.exe %SCRIPTS% || exit 1
